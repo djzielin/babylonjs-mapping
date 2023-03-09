37a2b38f613ba1ca23715080fdf8bef830b6b954
David Zielinski <djzielin@gmail.com>
2023-03-09 13:30:49 -0500

    tweaking parameter order

--- load_lib.js
+++ load_lib.js
@@ -1,8 +1,8 @@
 //var filePrefix="https://raw.githubusercontent.com/djzielin/babylonjs-mapping/main/lib/"; //causes MIME type error
-var filePrefix="https://cdn.jsdelivr.net/gh/djzielin/babylonjs-mapping";
+var filePrefix = "https://cdn.jsdelivr.net/gh/djzielin/babylonjs-mapping";
 
 
-var allFiles=[
+var allFiles = [
     //"/lib/Attribution.js",
     //"/lib/Buildings.js",
     //"/lib/BuildingsOSM.js",
@@ -15,42 +15,42 @@ var allFiles=[
     "/lib/TileSet.js"
 ];
 
-var numScriptsLoaded=0;
+var numScriptsLoaded = 0;
 
 //per documentation at: https://doc.babylonjs.com/toolsAndResources/thePlayground/externalPGAssets
 //per example at: https://playground.babylonjs.com/#WF3VKZ
 
 //TODO: check if script already on page to prevent double loading:
 // https://stackoverflow.com/questions/9659265/check-if-javascript-script-exists-on-page
-function loadSingleScript(url, callbackFunction,attachPoint){
+function loadSingleScript(url, attachPoint, callbackFunction) {
     console.log("trying to load: " + url);
     var s = document.createElement("script");
     //s.type = "text/javascript";
     //s.type="module"; //per https://stackoverflow.com/questions/42237388/syntaxerror-import-declarations-may-only-appear-at-top-level-of-a-module
-    s.type="text/javascript"; //see if this works?
+    s.type = "text/javascript"; //see if this works?
     s.src = url;
-    attachPoint.appendChild(s); //should this be head or body?
+    attachPoint.head.appendChild(s); //should this be head or body?
 
-    s.onload = function() {
+    s.onload = function () {
         console.log(url + " has been loaded!");
-	    numScriptsLoaded++;
+        numScriptsLoaded++;
         checkIfAllLoaded(callbackFunction);
     }
 }
 
-function checkIfAllLoaded(callbackFunction){
-    if(numScriptsLoaded==allFiles.length){
-        console.log("all babylonjs-mapping scripts are loaded!"); 
+function checkIfAllLoaded(callbackFunction) {
+    if (numScriptsLoaded == allFiles.length) {
+        console.log("all babylonjs-mapping scripts are loaded!");
         callbackFunction();
-    } else{
-        console.log("only have: " + numScriptsLoaded + " loaded out of: " + allFiles.length);
+    } else {
+        console.log("not done yet. only have: " + numScriptsLoaded + " loaded out of: " + allFiles.length);
     }
 }
 
-function loadAllMappingScripts(commitVer,callbackFunction,attachPoint){
+function loadAllMappingScripts(commitVer, attachPoint, callbackFunction) {
     console.log("trying to load all babylonjs-mapping scripts");
-    for(const script of allFiles){
-        loadSingleScript(filePrefix+commitVer+script,callbackFunction,attachPoint);
+    for (const script of allFiles) {
+        loadSingleScript(filePrefix + commitVer + script, attachPoint, callbackFunction);
     }
 }
 
