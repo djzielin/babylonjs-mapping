b2d0e1649ba7a7c6a1531ca3bb024c1ef15d19bf
David Zielinski <djzielin@duke.edu>
2024-07-24 17:24:13 -0400

    trying to fix OSM buildings API change

--- src/BuildingsOSM.ts
+++ src/BuildingsOSM.ts
@@ -8,6 +8,7 @@ import Buildings from "./Buildings";
 
 export default class BuildingsOSM extends Buildings {
     private serverNum = 0;
+    public accessToken: string = ""; //new for 2024 osmbuildings seem to now be onegeo
 
     constructor(tileSet: TileSet) {
         super("OSM", tileSet);
@@ -19,13 +20,21 @@ export default class BuildingsOSM extends Buildings {
         this.tileSet.ourAttribution.addAttribution("OSMB");
     }
 
-    private osmBuildingServers: string[] = ["https://a.data.osmbuildings.org/0.2/anonymous/tile/",
-        "https://b.data.osmbuildings.org/0.2/anonymous/tile/",
-        "https://c.data.osmbuildings.org/0.2/anonymous/tile/",
-        "https://d.data.osmbuildings.org/0.2/anonymous/tile/"];
+    private osmBuildingServers: string[] = [
+        "https://a-data.onegeo.co/maps/tiles/", //new for 2024 osmbuildings seem to now be onegeo
+        "https://b-data.onegeo.co/maps/tiles/",
+        "https://c-data.onegeo.co/maps/tiles/",
+        "https://d-data.onegeo.co/maps/tiles/"
+        
+        //"https://a.data.osmbuildings.org/0.2/anonymous/tile/",
+        //"https://b.data.osmbuildings.org/0.2/anonymous/tile/",
+        //"https://c.data.osmbuildings.org/0.2/anonymous/tile/",
+        //"https://d.data.osmbuildings.org/0.2/anonymous/tile/"
+    ];
 
     protected stripFilePrefix(original: string): string {
-        const stripped = original.slice(51);
+        const prefixLength=35; //51
+        const stripped = original.slice(prefixLength);
         //console.log("new file URL is: " + stripped);
         return stripped;
     }
@@ -38,7 +47,7 @@ export default class BuildingsOSM extends Buildings {
 
         const storedCoords = tile.tileCoords.clone();
 
-        const url = this.osmBuildingServers[this.serverNum] + storedCoords.z + "/" + storedCoords.x + "/" + storedCoords.y + ".json";
+        const url = this.osmBuildingServers[this.serverNum] + storedCoords.z + "/" + storedCoords.x + "/" + storedCoords.y + ".json"+"token="+this.accessToken;
         this.serverNum = (this.serverNum + 1) % this.osmBuildingServers.length; //increment server to use with wrap around
 
         const request: BuildingRequest = {
