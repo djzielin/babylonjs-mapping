b509978f0bff6ebcc0c55754abc944f9382dedea
David Zielinski <djzielin@duke.edu>
2024-10-16 12:45:25 -0400

    update math example

--- examples-local/math-test/src/index.ts
+++ examples-local/math-test/src/index.ts
@@ -8,7 +8,6 @@
 import { Vector2 } from "@babylonjs/core/Maths/math.vector";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 import TileMath from "../../../lib/TileMath";
-import TileSet from "../.../../lib/TileSet";
 
 class Game {
     
@@ -42,9 +41,7 @@ class Game {
         var tm=new TileMath(undefined);
 
         this.print("Running some math tests...");
-
-        //const ts=
-        
+       
         //https://epsg.io/transform#s_srs=4326&t_srs=3857&x=-80.0000000&y=35.0000000
         
         var webMercatorAnswer=new Vector2(-8905559.263461886,4163881.144064293);
@@ -53,12 +50,12 @@ class Game {
         this.print("===================================================");
         this.print("Lets test converting 4326 vs 3857");
 
-        var result1=tm.epsg3857toEpsg4326(webMercatorAnswer);
+        var result1=tm.epsg3857_to_Epsg4326(webMercatorAnswer);
         this.print("Calcd 3857 to 4326: " + result1);
         this.print("   difference: " + LatLonAnswer.subtract(result1));
         this.print("");
 
-        var result2=tm.epsg4326toEpsg3857(LatLonAnswer);
+        var result2=tm.epsg4326_to_Epsg3857(LatLonAnswer);
         this.print("Calcd 4326 to 3857: " + result2); 
         this.print("   difference: " + webMercatorAnswer.subtract(result2));
         this.print("===================================================");
@@ -66,7 +63,7 @@ class Game {
         var tileTest=new Vector3(18049,25907,16);
         this.print("looking at tile: " + tileTest);
         
-        var result=tm.tile2lonlat(tileTest);
+        var result=tm.tile_to_lonlat(tileTest);
         this.print("lon/lat: " + result);   
     }   
 
