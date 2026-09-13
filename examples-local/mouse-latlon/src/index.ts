0fb22f99433b16c20d5ed9a8e5b36ceb3ed766b8
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-09-13 00:41:22 -0400

    Enable pointer movement picking in the coordinate demo

--- examples-local/mouse-latlon/src/index.ts
+++ examples-local/mouse-latlon/src/index.ts
@@ -136,6 +136,7 @@ class Game {
         this.ourTS = new TileSet(this.scene, this.engine);
         this.ourTS.setRasterProvider(new RasterOSM(this.ourTS)); //raster basemap to OSM
         this.ourTS.createGeometry(new Vector2(4, 4), 20, 2); //4x4 tile set, 20m width of each tile, and 2 divisions on each tile
+        for (const tile of this.ourTS.ourTiles) tile.mesh.enablePointerMoveEvents = true;
         this.ourTS.updateRaster(35.2258461, -80.8400777, 16); //lat, lon, zoom. takes us to charlotte. 
 
         this.ourTileMath=new TileMath(this.ourTS);
